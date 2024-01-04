b676009543de85baa8e639cbb2051714298ea0c3
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-01-04 19:36:52 +0700

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,69 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import model.BirdImage
+
+@Composable
+fun App() {
+    MaterialTheme {
+        val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
+        BirdsPage(birdsViewModel)
+    }
+}
+
+@Composable
+fun BirdsPage(viewModel: BirdsViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    AnimatedVisibility(uiState.images.isNotEmpty()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 4.dp),
+            content = {
+                items(uiState.images) {
+                    BirdImageCell(it)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        asyncPainterResource("https://sebi.io/demo-image-api/${image.path}"),
+        "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    )
+}
